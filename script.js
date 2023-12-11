fbce58b23d50dcbbe695f5269e0d26ecc88c6e7e
mrunal3006 <128721188+mrunal3006@users.noreply.github.com>
2023-12-11 20:00:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function enterMetaverse() {
+	var statusElement = document.getElementById('status');
+
+	statusElement.textContent= 'Entered Metaverse';
+
+	var h1Element = document.createElement('h1');
+
+		h1Element.textContent = 'Entered Metaverse';
+
+	statusElement.parentNode.replaceChild(h1Element, statusElement);
+}
